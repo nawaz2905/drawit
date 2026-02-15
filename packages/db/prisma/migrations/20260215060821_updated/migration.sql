03fde8ced5eef6649e77b705c47064cddca9cd06
Nawaz Khan <abunawaz2905@gmail.com>
2026-02-15 11:40:32 +0530

    db updated

---
+++ packages/db/prisma/migrations/20260215060821_updated/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "Chat" ADD COLUMN     "endX" INTEGER,
+ADD COLUMN     "endY" INTEGER,
+ADD COLUMN     "startX" INTEGER,
+ADD COLUMN     "startY" INTEGER,
+ADD COLUMN     "type" TEXT;
